4efe315902221033f3dbb4a34fe7e318d1b3bdaf
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2022-01-30 23:19:53 -0500

    Run As per assignment requirment

--- N_Patel_TravelAgency/src/app/app.component.ts
+++ N_Patel_TravelAgency/src/app/app.component.ts
@@ -8,6 +8,26 @@ import { TourList } from './helper-files/tour-list';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'N_Patel_TravelAgency';
+  AllTours: TourList; 
+  constructor(){
+    let Swiss : Tour;
+    Swiss={
+      id: 2,
+      title: "Switzerland",
+      description: "fdggfhgjhgg",
+      creator: "dfa",
+      imgURL: "afadfaf",
+    };
+    this.consoleTour(Swiss);
+    this.AllTours = new TourList(Swiss);
+    console.log(this.AllTours.items);
+
+  }
+  consoleTour(tour: Tour):string{
+    console.log(tour.title);
+    return tour.title;
+  }
   
 }
