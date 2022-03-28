dc9ad7c5960ec5e628bf11449580d2eada71fe6f
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2022-03-27 21:54:23 -0400

    Update modify-content-component.component.ts

--- N_Patel_TravelAgency/src/app/modify-content-component/modify-content-component.component.ts
+++ N_Patel_TravelAgency/src/app/modify-content-component/modify-content-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Tour } from '../helper-files/tour-interface';
 
 @Component({
@@ -11,6 +11,26 @@ export class ModifyContentComponentComponent implements OnInit {
   @Output() newTourAdd: EventEmitter<Tour> = new EventEmitter<Tour>();
   @Output() updateTourAdd: EventEmitter<Tour> = new EventEmitter<Tour>();
   newTour?: Tour;
+  @ViewChild('title')
+  inputTitle!: { nativeElement: { value: string; }; };
+
+  @ViewChild('id')
+  inputId!: { nativeElement: { value: string; }; };
+  
+  @ViewChild('description')
+  inputDesc!: { nativeElement: { value: string; }; };
+  
+  @ViewChild('price')
+  inputPrice!: { nativeElement: { value: string; }; };
+  
+  @ViewChild('type')
+  inputType!: { nativeElement: { value: string; }; };
+  
+  @ViewChild('tags')
+  inputTag!: { nativeElement: { value: string; }; };
+
+  @ViewChild('imageUrl')
+  inputImg!: { nativeElement: { value: string; }; };
   
   constructor() { }
 
@@ -28,6 +48,13 @@ export class ModifyContentComponentComponent implements OnInit {
       tags : tags.split(",")
     };
     this.newTourAdd.emit(this.newTour);
+    this.inputTitle.nativeElement.value = '';
+    this.inputId.nativeElement.value = '';
+    this.inputDesc.nativeElement.value = '';
+    this.inputPrice.nativeElement.value = '';
+    
+    this.inputTag.nativeElement.value = '';
+    this.inputImg.nativeElement.value = '';
   }
   updateTour(id:string, title:string,imgURL:string,description:string,price:string,type:string,tags:string): void{
     this.newTour = {
@@ -40,6 +67,13 @@ export class ModifyContentComponentComponent implements OnInit {
       tags : tags.split(",")
     };
     this.updateTourAdd.emit(this.newTour);
+    this.inputTitle.nativeElement.value = '';
+    this.inputId.nativeElement.value = '';
+    this.inputDesc.nativeElement.value = '';
+    this.inputPrice.nativeElement.value = '';
+    
+    this.inputTag.nativeElement.value = '';
+    this.inputImg.nativeElement.value = '';
   }
 
 }
